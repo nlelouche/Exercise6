82f005b1b5d3a493233bd090973fa95239c64c02
NLelouche <nahuel.lelouche@threepillarglobal.com>
2012-03-01 10:54:16 -0300

    First commit

---
+++ Exercise6/Exercise6/Ex06AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Ex06AppDelegate.h
+//  Exercise6
+//
+//  Created by TPG on 3/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Ex06AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Exercise6/Exercise6/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Exercise6
+//
+//  Created by TPG on 3/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Ex06AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Ex06AppDelegate class]));
+    }
+}
