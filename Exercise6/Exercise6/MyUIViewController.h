8336f1f4c819eddb1ecee24467df433542f7df1a
NLelouche <nahuel.lelouche@threepillarglobal.com>
2012-03-05 17:54:06 -0300

    .

---
+++ Exercise6/Exercise6/MyUIViewController.h
@@ -0,0 +1,17 @@
+//
+//  MyUIViewController.h
+//  Exercise6
+//
+//  Created by TPG on 3/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyUIViewController : UIViewController 
+
+
+@property (nonatomic, retain) IBOutlet UIImageView *myImageView;
+@property (nonatomic, retain) IBOutlet UILabel *myLabel;
+@property (nonatomic, retain) IBOutlet UIPickerView *myPickerView;
+@end
---
+++ Exercise6/Exercise6/MyUIViewController.m
@@ -0,0 +1,61 @@
+//
+//  MyUIViewController.m
+//  Exercise6
+//
+//  Created by TPG on 3/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyUIViewController.h"
+
+@implementation MyUIViewController
+
+@synthesize myImageView;
+@synthesize myLabel;
+@synthesize myPickerView;
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    
+    return self;
+}
+
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
